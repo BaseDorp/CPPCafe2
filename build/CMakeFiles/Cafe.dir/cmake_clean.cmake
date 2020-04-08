e83364135f12eccd141c79b0456bfde3cfb96d2e
BaseDorp <shirsch@colum.edu>
2020-04-08 10:59:08 -0500

    adds build files and changes to pay function

---
+++ build/CMakeFiles/Cafe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cafe.dir/cafe.cpp.o"
+  "Cafe.pdb"
+  "Cafe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cafe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cafe.cpp
+++ cafe.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 
 float GetTotal(std::vector<float> _cart);
-void Pay(float total);
+std::vector<int> Pay(float total, int pay);
+
 
 int main()
 {
@@ -19,9 +20,6 @@ int main()
 
 	std::vector<float> Cart;
 
-    // TODO remove debug for paying
-    Pay(10);
-
 	while (isBuying)
 	{
 		// Loop to that repeats until user makes a valid input
@@ -37,33 +35,40 @@ int main()
 
 		switch (itemNum)
 		{
-		case 1:
-			std::cout << "Added Coffee to cart" << std::endl;
-			Cart.push_back(coffeePrice);
+			case 1:
+				std::cout << "Added Coffee to cart" << std::endl;
+				Cart.push_back(coffeePrice);
 			break;
-		case 2:
-			std::cout << "Added Tea to cart" << std::endl;
-			Cart.push_back(teaPrice);
+			case 2:
+				std::cout << "Added Tea to cart" << std::endl;
+				Cart.push_back(teaPrice);
 			break;
-		case 3:
-			std::cout << "Added Croissant to cart" << std::endl;
-			Cart.push_back(croissantPrice);
+			case 3:
+				std::cout << "Added Croissant to cart" << std::endl;
+				Cart.push_back(croissantPrice);
 			break;
-		case 4:
-			std::cout << "Added Hot Chocolate to cart" << std::endl;
-			Cart.push_back(hotChocolatePrice);
+			case 4:
+				std::cout << "Added Hot Chocolate to cart" << std::endl;
+				Cart.push_back(hotChocolatePrice);
 			break;
-		case 5:
-			if (Cart.size() <= 0)
-			{
-				isBuying = false;
-			}
-			else
-			{
-				std::cout << "Your total is $" << GetTotal(Cart) << std::endl;
-                Pay(GetTotal(Cart));
-				isBuying = false;
-			}
+			case 5:
+				if (Cart.size() <= 0)
+				{
+					isBuying = false;
+				}
+				else
+				{
+					std::cout << "Your total is $" << GetTotal(Cart) << std::endl;
+
+					uint balance = 0;
+					std::cout << "How much would you like to pay (Only whole numbers)" << std::endl;
+    				std::cin >> balance;
+    				// TODO make sure the pay is a whole number
+    				std::cout << balance;
+					Pay(GetTotal(Cart), balance);
+
+					isBuying = false;
+				}
 			break;
 		}
 	}
@@ -86,23 +91,37 @@ float GetTotal(std::vector<float> _cart)
 	return total;
 }
 
-void Pay(float total)
+std::vector<int> Pay(float total, int pay)
 {
-    float pay = 0;
-    int tempBalance = 0;
-    int x = 0;
-    int bills[] = { 10000, 2000, 1000, 500, 100, 25, 10, 5, 1 };
+	// Gets how much the user is owed back
+	float change = pay - total;
 
-    std::cout << "How much would you like to pay (Only whole numbers)" << std::endl;
-    std::cin >> tempBalance;
-    // TODO make sure the pay is a whole number
-    std::cout << tempBalance;
+	// Removes the decimal
+	total = total * 10;
+	change = change * 10;
+	
+	
+	// All money values x10 so there is no decimal
+    std::vector<int> bills = { 10000, 2000, 1000, 500, 100, 25, 10, 5, 1 };
+	// extra element for return pay
+	std::vector<int> returnBills = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	// 
+	std::vector<std::string> billsName = { "hundred", "twenty", "ten", "five", "dollar", "quarter", "dime", "nickle", "penny"};
 
-    tempBalance = total - tempBalance;
+	// Goes through the vector for each bill
+	for (int i = 0; i < bills.size(); i++)
+	{
+		// Only does math is the change is greater than the current bill
+		if (change > bills[i])
+		{
+			// counts how many bills of that type is needed
+			returnBills[i] = (int)change % bills[i];
+			std::cout << returnBills[i/10];
+		}
+	}
+
+	return returnBills;
     
-    x = tempBalance % bills[0];
-    pay = x * bills[0];
-
     // customer can only pay in 1,5,10,20
     // change must be least number of bills and coins possible
     // must have 3 unit tests
